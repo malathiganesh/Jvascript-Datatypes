040ed8400555dbb0be7101a79a0bd2c252e5cc57
malathiganesh <107910492+malathiganesh@users.noreply.github.com>
2022-09-09 14:43:02 +0530

    JavaScript Datatypes

---
+++ data.js
@@ -0,0 +1,31 @@
+var a=4;
+var b='hh';
+let c=a*b;
+console.log( a);
+var a=null;
+console.log(typeof a);
+var a=15959595655985998555n;
+console.log(typeof a);
+var a=[1,2,3,4];
+console.log(typeof a);
+var a=2;
+console.log(typeof a);
+var person={
+    name:'malathi',
+    age:22
+}
+console.log(typeof person);
+
+
+// function myFunc(who){
+// if(!who){
+//   return null;
+// }
+// else{
+//     return{
+//         message:console.log('welcome',who)
+//     }
+// }
+// }
+// myFunc('Malathi');
+// myFunc();
